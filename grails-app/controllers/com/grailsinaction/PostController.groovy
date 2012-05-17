64e05e1abae6b65d99076f09276e0d3facad0850
Alejandro Giorgi <ale@pangea.(none)>
2012-05-17 15:53:40 -0300

    Fin punto 5.1 Controller essentials

--- grails-app/controllers/com/grailsinaction/PostController.groovy
+++ grails-app/controllers/com/grailsinaction/PostController.groovy
@@ -3,4 +3,33 @@ package com.grailsinaction
 class PostController {
 
     def scaffold = true
+	
+	def index = {
+		if (!params.id) 
+			params.id = "eze.giorgi"	
+		redirect(action: 'timeline',params: params)		
+	}
+	
+	def timeline = {
+		def user = User.findByUserId(params.id)
+		[ user : user ]
+	}
+	
+	def addPost = {
+		def user = User.findByUserId(params.id)
+		if (user) {
+			def post = new Post(params)
+			user.addToPosts(post)
+			if (user.save()) {
+				flash.message = "Successfully created Post"
+			} else {
+				user.discard()
+				flash.message = "Invalid or empty post"
+			}
+		} else {
+			flash.message = "Invalid User Id"
+		}
+		
+		redirect(action: 'timeline', id: params.id)
+	}
 }
